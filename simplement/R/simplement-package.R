b167ceb90c64fc4ad3ec423a92866b6f3ed9f52c
sangwonh <sangwonh@Sangwons-MacBook-Air.local>
2024-06-19 15:06:00 -0700

    first commit

---
+++ simplement/R/simplement-package.R
@@ -0,0 +1,11 @@
+#' flowtrend
+#'
+#' This package implements the `flowtrend` method for automatic gating of flow cytometry data using trend filtering.
+#' 
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
